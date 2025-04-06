ff458000fbe638d865a53068052b60aaa46ef2bb
sikakamura <you@example.com>
2025-04-06 10:32:20 +0700

    change the web part

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Job_Portal/',
+  base: 'Job_Portal',
   plugins: [vue(),tailwindcss()],
 })
 
